7591fced91b70c7ac2d2f6bd6019f630f8f54674
sjetley <saumya.jetley@ox.ac.uk>
2017-06-13 16:39:35 +0100

    removing all modules besides adversarial

--- GUI/README.md
+++
@@ -1,3 +0,0 @@
-# Some snippets for playing with the Qt GUI
-
-Run code as `qlua fileName.lua`. To create the `ui` files, you need to install *Qt Designer*.
--- Video-decoder/libgl.h
+++
@@ -1,5 +0,0 @@
-int CreateWindow(const char *title, int x, int y, int w, int h);
-void StartWindow();
-int GetWindowSize(int *w, int *h);
-int Blt(const void *image, int fmt, int w, int h, int dx, int dy, int dw, int dh);
-int Present();
