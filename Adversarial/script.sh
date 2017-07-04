7c049fefc257c2cd59c9195d70cf4f78933a7e2a
sjetley <saumya.jetley@ox.ac.uk>
2017-07-04 11:42:57 +0100

    more experiments

--- Adversarial/script.sh
+++ Adversarial/script.sh
@@ -47,7 +47,7 @@ export platformtype='cuda'
 th ./main.lua | tee runtimerecord.txt
 END
 
-
+:<<'END'
 #Test for CUBS-200 - basic
 export action="evaluate"
 export mode='unproc'
@@ -67,7 +67,7 @@ export gpumode=1
 export gpusetdevice=1
 export platformtype='cuda'
 th ./main.lua | tee runtimerecord.txt
-
+END
 
 
 :<<'END'
@@ -78,7 +78,7 @@ export path_model='{"#models/cubs-2level-1global/mlocal_1.net","#models/cubs-2le
 export atten=2
 export batch_size=2
 export image_size=80
-export noise_intensity=8
+export noise_intensity=0
 export path_save='#dataset/cubs-adv'
 export path_img='#dataset/cubs-200.t7'
 #export path_label='#dataset/label_gt.lua'
@@ -139,3 +139,25 @@ export gpusetdevice=1
 export platformtype='cuda'
 th ./main.lua | tee runtimerecord.txt
 END
+
+# Test for Imagenet
+export action='evaluate'
+export mode='unproc'
+export path_model='{"#models/imagenet-vgg19/VGG_ILSVRC_19_layers_deploy.prototxt","#models/imagenet-vgg19/VGG_ILSVRC_19_layers.caffemodel"}'
+export type_model='caffe'
+export atten=0
+export batch_size=50
+export image_size=224
+export noise_intensity=0
+export path_save='#dataset/imagenet-adv'
+#export path_img='#dataset/cubs-200.t7'
+export path_label='#dataset/imagenet/label_valgt.lua'
+export path_img='#dataset/imagenet/image_valgt.lua'
+#export list_labels='#dataset/overfeat_label.lua'
+export mean='{123.68/255 , 116.779/255, 103.939/255}' # global mean used to train the network
+export std='{1,1,1}'  # global std used to train the network
+export gpumode=1
+export gpusetdevice=1
+export platformtype='cuda'
+th ./main.lua | tee runtimerecord.txt
+#-------results for fwd run--------#
